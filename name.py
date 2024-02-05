3e96c68a52807f9935262c6451e3eddb921750db
MoshopeOmotoso <157934041+MoshopeOmotoso@users.noreply.github.com>
2024-02-04 19:09:30 -0600

    Update name.py

--- name.py
+++ name.py
@@ -1,19 +1,13 @@
 def draw(card_rank):
   if card_rank == 1:
-    # A 1 stands for an ace.
     card_name = "Ace"
   elif card_rank == 11:
-    # An 11 stands for a jack.
     card_name = "Jack"
   elif card_rank == 12:
-    # A 12 stands for a queen.
     card_name = "Queen"
   elif card_rank == 13:
-    # A 13 stands for a king.
     card_name = "King"
   else:
-    # All other cards are named by their
-    # number, or rank.
     card_name = str(card_rank)
 
   if card_rank == 1 or card_rank == 8:
