6f5b5634ebd05818c4e9f14a4458f2f5a42cf565
MoshopeOmotoso <157934041+MoshopeOmotoso@users.noreply.github.com>
2024-02-04 19:09:58 -0600

    Update value.py

--- value.py
+++ value.py
@@ -3,12 +3,8 @@ def card_value(card_rank):
     return None
 
   if card_rank == 11 or card_rank == 12 or card_rank == 13:
-    # Jacks, Queens, and Kings are worth 10.
     return 10
   elif card_rank == 1:
-    # Aces are worth 11.
     return 11
   else:
-    # All other cards are worth the same as
-    # their rank.
     return card_rank
